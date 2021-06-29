32b816239d6bc2721a2afbc0d2726eaaf502ab03
infosergio2020 <infosergio2020@gmail.com>
2021-06-29 17:05:29 -0300

    se implemento una clase para poder manipular los productos a vender para mas detalle ver bitacora

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /*     
-    
-____________________________________
+__________________________________
 / Si necesitas ayuda, contáctame   \
  ------------------------------------
         \   ^__^
@@ -11,107 +10,172 @@ ____________________________________
 Creado por oriana. Este encabezado debe mantenerse intacto,
 excepto si este es un proyecto de un estudiante.
 */
-document.addEventListener("DOMContentLoaded", () => {
 
-    var lista = []
-    var total = 0;
+class Producto{
+    constructor(titulo,cantidad,precio){
+        this.id = randHex(12);
+        this.titulo = titulo;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    getId(){return this.id}
+    setTitulo(titulo){this.titulo=titulo;}
+    getTitulo(){return this.titulo;}
+    setPrecio(precio){this.precio = precio;}
+    getPrecio(){ return this.precio;}
+    setCantidad(cantidad){ this.cantidad = cantidad;}
+    getCantidad(){ return this.cantidad;} 
+    calcularSubtotal(){ return (this.cantidad*this.precio)}
+}
 
-    // Escuchamos el click del botón
-    const $boton = document.querySelector("#btnCrearPdf");
-    $boton.addEventListener("click", () => {
+class Boleta {
+    constructor(total,productos){
+        this.total = total;
+        this.productos = productos;
+    }
+    agregarProducto(producto){ this.productos.push(producto); }
 
-        const $elementoParaConvertir = document.querySelector('#comprobante'); // <-- Aquí puedes elegir cualquier elemento del DOM
-        html2pdf()
-            .set({
-                margin: 1,
-                filename: 'documento.pdf',
-                image: {
-                    type: 'jpeg',
-                    quality: 0.98
-                },
-                html2canvas: {
-                    scale: 3, // A mayor escala, mejores gráficos, pero más peso
-                    letterRendering: true,
-                },
-                jsPDF: {
-                    unit: "in",
-                    format: "a3",
-                    orientation: 'portrait' // landscape o portrait
-                }
-            })
-            .from($elementoParaConvertir)
-            .save()
-            .catch(err => console.log(err));
-    });
+    buscarIndiceProducto(pid){ return this.productos.findIndex(x => x.id === pid); }
 
-    const $botonAgregar = document.querySelector("#btnAgregar");
-    $botonAgregar.addEventListener("click", () => {
-        var nombre = document.getElementById("IdNombre").value;
-        var detalle = document.getElementById("idDetalle").value;
-        var precio = document.getElementById("IdPrecio").value;
-        var unidad = document.getElementById("IdUnidad").value;
-        console.log(nombre);
-        console.log(detalle);
-        console.log(unidad);
-        console.log(precio);
+    actualizarProducto(producto){
+        this.productos[this.buscarIndiceProducto(producto.getId())].setCantidad(producto)
+    }
 
-        lista.push({ nombre, detalle, unidad, precio });
+    eliminarProducto(pid){
+        this.productos.splice(this.buscarIndiceProducto(pid),1);
+        console.log("producto eliminado");
+    }
 
-        //intentar agregar elementos en el ul del DOM
-        var tbody = document.querySelector("#resultado");
-
-
-        tr = document.createElement('tr');
-
-        td1 = document.createElement('td');
-        td2 = document.createElement('td');
-        td3 = document.createElement('td');
-        td4 = document.createElement('td');
-
-
-
-        td1.appendChild(document.createTextNode("" + nombre));
-        td2.appendChild(document.createTextNode("" + detalle));
-        td3.appendChild(document.createTextNode("" + unidad));
-        td4.appendChild(document.createTextNode("" + precio));
-
-
-        // <input type="submit" id="editar" value="Editar">
-
-
-
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-
-
-
-        tbody.appendChild(tr);
-
-
-        total = total + (precio * unidad)
-        document.getElementById("MontoTotal").innerHTML = "$" + total;
-
-
-        lista.forEach(element => {
-            console.log(element);
-        });
-
-        // IdUnidad
-        // IdPrecio
-        // idDetalle
-        // IdNombre
-
-    });
-
-    const $botonOcultar = document.querySelector("#btnOcultar");
-    $botonOcultar.addEventListener("click", () => {
-        var x = document.getElementById("formulario");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
+    calcularTotal(){
+        this.total=0
+        if (this.productos.length != 0) {
+            this.productos.forEach(producto => { this.total += producto.calcularSubtotal(); });
+            return this.total;
         }
-    });
-});
+        return 0;
+    }
+}
+
+// --------------------------------------------------------
+//------------------SECCION DE EVALUACION------------------
+// --------------------------------------------------------
+
+
+
+// prueba de la funcion agregar productos
+const miboleta = new Boleta(0,[]);
+// de obj JS a JSON Text
+// var myJSONText = JSON.stringify(obj);
+// de JSON a obj JS  Text
+// var obj = JSON.parse(myJSONtext);
+// console.log(miboleta.productos);
+
+
+let aux = JSON.stringify(miboleta.productos);
+document.addEventListener("DOMContentLoaded", function (event) {
+     // Escuchamos el click del botón
+     const $boton_guardar = document.querySelector("#btnCrearPdf");
+     $boton_guardar.addEventListener("click", () => {
+ 
+         const $elementoParaConvertir = document.querySelector('#comprobante'); // <-- Aquí puedes elegir cualquier elemento del DOM
+         html2pdf()
+             .set({
+                 margin: 1,
+                 filename: 'documento.pdf',
+                 image: {
+                     type: 'jpeg',
+                     quality: 0.98
+                 },
+                 html2canvas: {
+                     scale: 3, // A mayor escala, mejores gráficos, pero más peso
+                     letterRendering: true,
+                 },
+                 jsPDF: {
+                     unit: "in",
+                     format: "a3",
+                     orientation: 'portrait' // landscape o portrait
+                 }
+             })
+             .from($elementoParaConvertir)
+             .save()
+             .catch(err => console.log(err));
+     });
+
+     const $boton_agregar = document.querySelector("#btnAgregar");
+     $boton_agregar.addEventListener("click", agregar)
+})
+
+
+function agregar(){
+    titulo = document.querySelector("#titulo").value
+    cantidad = parseInt(document.querySelector("#cantidad").value,10);
+    precio = parseInt(document.querySelector("#precio").value,10);
+
+    nuevoProducto = new Producto(titulo,cantidad,precio);
+    miboleta.agregarProducto(nuevoProducto);
+    tbody = document.querySelector("#mytbody");
+    fila = construirFila(nuevoProducto);
+    tbody.appendChild(fila);
+
+    document.getElementById("MontoTotal").innerHTML = "$" + miboleta.calcularTotal();
+    console.log(miboleta.productos);
+}
+    
+
+
+function construirFila(producto){
+    button = document.createElement('button');
+    button.innerHTML = "BORRAR";
+    button.classList.add("btn");
+    button.classList.add("btn-danger");
+    button.onclick = borrar 
+
+    tr = document.createElement('tr');
+    tr.setAttribute("data-product-id", producto.getId());
+
+    td1 = document.createElement('td');
+    td2 = document.createElement('td');
+    td3 = document.createElement('td');
+    td4 = document.createElement('td');
+    td5 = document.createElement('td');
+
+    
+    td1.appendChild(document.createTextNode(producto.titulo));
+    td2.appendChild(document.createTextNode(producto.cantidad));
+    td3.appendChild(document.createTextNode(producto.precio));
+    td4.appendChild(document.createTextNode(producto.calcularSubtotal()));
+    td5.appendChild(button);
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+
+    return tr
+}
+
+
+
+//  random hex string generator
+var randHex = function(len) {
+    var maxlen = 8,
+        min = Math.pow(16,Math.min(len,maxlen)-1) 
+        max = Math.pow(16,Math.min(len,maxlen)) - 1,
+        n   = Math.floor( Math.random() * (max-min+1) ) + min,
+        r   = n.toString(16);
+    while ( r.length < len ) {
+       r = r + randHex( len - maxlen );
+    }
+    return r;
+  };
+
+// BORRADO DE FILAS
+function borrar(e){
+    // e.target es el boton que se presionó
+    // 1er parentNode es el td que contiene al boton el 2do parentNode es tr
+    let p = e.target.parentNode.parentNode;    
+    miboleta.eliminarProducto(p.dataset['productId']);
+    p.parentNode.removeChild(p);
+    document.getElementById("MontoTotal").innerHTML = "$" + miboleta.calcularTotal();
+}
